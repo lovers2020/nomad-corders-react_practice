c370e0cb8507782694048390aef7f3f74feefd01
lovers2020 <dkrkskswl@naver.com>
2023-10-22 22:35:11 +0900

    Update File

---
+++ src/TodoList.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [todo, setTodo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setToDos((currentArray) => [todo, ...currentArray]);
+    setTodo("");
+  };
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={todo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((toDo, index) => (
+          <li key={index}>{toDo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default App;
