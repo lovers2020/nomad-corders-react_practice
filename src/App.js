cbe42777cd2bbd33613f0a02c927d8a6613ef6fd
lovers2020 <dkrkskswl@naver.com>
2023-10-23 21:27:25 +0900

    Update file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { useState, useEffect } from "react";
 import Movie from "./components/Movie.js";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./routes/Home.js";
 import Detail from "./routes/Detail.js";
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route
-          path={`{$process.env.PUBLIC_URL +}/movie/:id`}
+          path={`${process.env.PUBLIC_URL}/movie/:id`}
           element={<Detail />}
         />
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
